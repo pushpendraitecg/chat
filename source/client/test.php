0fbda149205b786f2b3fb96f321588bc8e7b0fef
pushpendraitecg <pushpendraitecg@gmail.com>
2014-01-06 17:31:34 +0530

    Create test.php

---
+++ source/client/test.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Magnoliyan Video Chat:3</title>
+
+        <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.2/css/bootstrap.min.css">
+        <link rel="stylesheet" href="mgVideoChat/mgVideoChat-1.0.3.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="page-header">
+                <h1>Magnoliyan Video Chat <small>Simple</small></h1>
+                <p>If no one else is in the room, please open the same page in other tab to do self-test.</p>
+            </div>
+            <div id="mgVideoChat"></div>
+        </div>
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script type="text/javascript" src="//netdna.bootstrapcdn.com/bootstrap/3.0.2/js/bootstrap.min.js"></script>
+        <!-- Video Chat -->
+        <script src="mgVideoChat/mgVideoChat-1.0.3-min.js"></script>
+        <script>
+            $(document).ready(function(){
+               // alert(3);
+            $('#mgVideoChat').mgVideoChat({
+                wsURL: 'ws://ec2-50-17-83-100.compute-1.amazonaws.com:8080?room=1'
+            });
+            
+            ////////////////////////////////////////////////////
+            
+            
+            
+            
+            
+            
+            }); /////// end
+            
+        </script>
+    </body>
+</html>
